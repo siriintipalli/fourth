30e93654a886ee08d3af597bed9f53311e1f12a3
GuruSainath <gurusainath007@gmail.com>
2019-07-01 16:38:39 +0530

    changes

--- src/store.js
+++ src/store.js
@@ -1,21 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        todoList: [],
+  state: {
+    todoList: [],
+  },
+  mutations: {
+    TASK_ADD: function(state, response) {
+      state.todoList = response;
     },
-    mutations: {
 
-        TASK_ADD: function(state, response) {
-            state.todoList.push(response);
-        }
-
-
+  },
+  actions: {
+    postTodoList: function(context, postData) {
+      return axios.post('https://checklister-api.herokuapp.com/item-list/', postData)
+        .then(function(response) {
+          console.log(response)
+          context.dispatch('getTodoList')
+          alert('data addes successfully')
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
     },
-    actions: {
-
+    getTodoList: function(context) {
+      return axios.get('https://checklister-api.herokuapp.com/item-list/')
+        .then(function (response) {
+          console.log(response.data)
+          context.commit('TASK_ADD', response.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
+  }
 })
--- src/views/Content.vue
+++ src/views/Content.vue
@@ -8,6 +8,11 @@
         <div class="form-group">
         <button  v-on:click="add()" class="btn btn-primary">Add task</button>
         </div>
+        <ul>
+            <li v-for="(list, index) in todoList" :key="index">
+                {{list.name}}
+            </li>
+        </ul>
     </div>
 </div>
 </template>
@@ -18,25 +23,28 @@ export default{
             addItem:null
         }
     },
-     computed:{
-         todoList: function(){
-             return this.$store.state.todoList
-         }
-     },
+    computed:{
+        todoList: function(){
+            return this.$store.state.todoList
+        }
+    },
+    mounted() {
+        this.$store.dispatch('getTodoList')
+    },
     methods: {
         add:function(){
         
-            this.$store.commit('TASK_ADD',this.addItem)
+            var data = {
+                "name": this.addItem,
+                "description": "des",
+                "is_complete": true,
+                "priority": "H",
+                "user": 1
+            }
+            console.log(data)
+            this.$store.dispatch('postTodoList', data)
             this.addItem=null
-            axios
-      .get('https://checklister-api.herokuapp.com/items/1/')
-      .then(response => (this.info = response))
         },
-   
-   
-        
-
-        
     }
 }
 </script>
